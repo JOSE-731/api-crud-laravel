8f0fe3e390c521b2ce9391668bc618e50932a59c
jose diaz <jose7312000@gmail.com>
2022-01-20 20:39:25 -0500

    Actualizando rutas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ArticuloController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,7 +18,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/*
 Route::get('/articulos', 'App/Http/Controllers/ArticuloController@index'); //retorna todos los datos
 Route::post('/articulos', 'App/Http/Controllers/ArticuloController@store'); //post para enviar
 Route::put('/articulos{id}', 'App/Http/Controllers/ArticuloController@update'); //put actualizar
-Route::delete('/articulos{id}', 'App/Http/Controllers/ArticuloController@destroy'); //para eliminar
+Route::delete('/articulos{id}', 'App/Http/Controllers/ArticuloController@destroy'); //para eliminar*/
+
+Route::get('/articulos', [ArticuloController::class,'index']);
+Route::post('/articulos', [ArticuloController::class,'store']);
+Route::put('/articulos/{id}', [ArticuloController::class,'update']);
+Route::delete('/articulos/{id}', [ArticuloController::class,'destroy']);
+
